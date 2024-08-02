2fc95643c5d984640857c1182d83576fbbdfbae0
LiuKaining1 <2659812228@qq.com>
2024-08-02 22:20:44 +0800

    Site updated: 2024-08-02 22:20:43

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1722608047864";
+const VERSION = "1722608442785";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
